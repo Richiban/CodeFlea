3685b2f7c120d920578d2f5833a958bb29d6bb38
Richard Gibson <richard.gibson@asos.com>
2019-12-31 10:42:44 +0000

    Refactoring

--- out/extension.js
+++ out/extension.js
@@ -3,56 +3,68 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
+function lineIsBoring(line) {
+    return !/[a-zA-Z0-9]/.test(line.text);
+}
+function lineIsIndentationChange(prevLine, currentLine) {
+    return (prevLine.firstNonWhitespaceCharacterIndex !==
+        currentLine.firstNonWhitespaceCharacterIndex);
+}
+function lineIsInteresting(prevLine, currentLine) {
+    if (lineIsBoring(currentLine))
+        return false;
+    if (!prevLine)
+        return true;
+    return (lineIsIndentationChange(prevLine, currentLine) || lineIsBoring(prevLine));
+}
+function moveCursorToBeginningOfLine(editor, line) {
+    editor.selection = new vscode.Selection(new vscode.Position(line.lineNumber, line.firstNonWhitespaceCharacterIndex), new vscode.Position(line.lineNumber, line.firstNonWhitespaceCharacterIndex));
+}
+function fromDirection(direction) {
+    return direction === "forwards" ? (x) => x + 1 : (x) => x - 1;
+}
+function* iterLines(document, currentLineNumber, direction) {
+    const advance = fromDirection(direction);
+    currentLineNumber = advance(currentLineNumber);
+    while (withinBounds()) {
+        const prevLine = currentLineNumber === 0
+            ? undefined
+            : document.lineAt(currentLineNumber - 1);
+        const currentLine = document.lineAt(currentLineNumber);
+        yield { prevLine, currentLine };
+        currentLineNumber = advance(currentLineNumber);
+    }
+    function withinBounds() {
+        return currentLineNumber >= 0 && currentLineNumber < document.lineCount;
+    }
+}
+function moveTo(direction) {
+    const editor = vscode.window.activeTextEditor;
+    if (editor && editor.selection.isEmpty) {
+        const position = editor.selection.active;
+        const documentLines = iterLines(editor.document, position.line, direction);
+        for (const { prevLine, currentLine } of documentLines) {
+            if (lineIsInteresting(prevLine, currentLine)) {
+                moveCursorToBeginningOfLine(editor, currentLine);
+                break;
+            }
+        }
+    }
+}
+function moveToNextInterestingLine() {
+    return moveTo("forwards");
+}
+function moveToPrevInterestingLine() {
+    return moveTo("backwards");
+}
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "codeflea" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand("extension.nextInterestingLine", () => {
-        // The code you place here will be executed every time your command is executed
-        var _a;
-        // Display a message box to the user
-        const editor = vscode.window.activeTextEditor;
-        if (editor && editor.selection.isEmpty) {
-            // the Position object gives you the line and character where the cursor is
-            const position = editor.selection.active;
-            vscode.window.showInformationMessage("You're on line " + position.line);
-            function lineIsBoring(line) {
-                return !/[a-zA-Z0-9]/.test(line.text);
-            }
-            function lineIsIndentationChange(prevLine, currentLine) {
-                return (prevLine.firstNonWhitespaceCharacterIndex !==
-                    currentLine.firstNonWhitespaceCharacterIndex);
-            }
-            function lineIsInteresting(currentLine) {
-                var _a;
-                if (lineIsBoring(currentLine)) {
-                    return false;
-                }
-                const prevLine = (_a = editor) === null || _a === void 0 ? void 0 : _a.document.lineAt(currentLine.lineNumber - 1);
-                if (!prevLine)
-                    return false;
-                return (lineIsIndentationChange(prevLine, currentLine) ||
-                    lineIsBoring(prevLine));
-            }
-            var currentLineNumber = position.line;
-            var currentLine = null;
-            while (currentLineNumber < editor.document.lineCount - 1) {
-                currentLineNumber++;
-                currentLine = editor.document.lineAt(currentLineNumber);
-                if (lineIsInteresting(currentLine)) {
-                    break;
-                }
-            }
-            const column = ((_a = currentLine) === null || _a === void 0 ? void 0 : _a.firstNonWhitespaceCharacterIndex) || 0;
-            editor.selection = new vscode.Selection(new vscode.Position(currentLineNumber, column), new vscode.Position(currentLineNumber, column));
-        }
-    });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand("extension.nextInterestingLine", moveToNextInterestingLine));
+    context.subscriptions.push(vscode.commands.registerCommand("extension.prevInterestingLine", moveToPrevInterestingLine));
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,97 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
+function lineIsBoring(line: vscode.TextLine) {
+  return !/[a-zA-Z0-9]/.test(line.text);
+}
+
+function lineIsIndentationChange(
+  prevLine: vscode.TextLine,
+  currentLine: vscode.TextLine
+) {
+  return (
+    prevLine.firstNonWhitespaceCharacterIndex !==
+    currentLine.firstNonWhitespaceCharacterIndex
+  );
+}
+
+function lineIsInteresting(
+  prevLine: vscode.TextLine | undefined,
+  currentLine: vscode.TextLine
+) {
+  if (lineIsBoring(currentLine)) return false;
+  if (!prevLine) return true;
+
+  return (
+    lineIsIndentationChange(prevLine, currentLine) || lineIsBoring(prevLine)
+  );
+}
+
+function moveCursorToBeginningOfLine(
+  editor: vscode.TextEditor,
+  line: vscode.TextLine
+) {
+  editor.selection = new vscode.Selection(
+    new vscode.Position(line.lineNumber, line.firstNonWhitespaceCharacterIndex),
+    new vscode.Position(line.lineNumber, line.firstNonWhitespaceCharacterIndex)
+  );
+}
+
+type Direction = "forwards" | "backwards";
+
+function fromDirection(direction: Direction) {
+  return direction === "forwards" ? (x: number) => x + 1 : (x: number) => x - 1;
+}
+
+function* iterLines(
+  document: vscode.TextDocument,
+  currentLineNumber: number,
+  direction: Direction
+) {
+  const advance = fromDirection(direction);
+  currentLineNumber = advance(currentLineNumber);
+
+  while (withinBounds()) {
+    const prevLine =
+      currentLineNumber === 0
+        ? undefined
+        : document.lineAt(currentLineNumber - 1);
+    const currentLine = document.lineAt(currentLineNumber);
+
+    yield { prevLine, currentLine };
+
+    currentLineNumber = advance(currentLineNumber);
+  }
+
+  function withinBounds() {
+    return currentLineNumber >= 0 && currentLineNumber < document.lineCount;
+  }
+}
+
+function moveTo(direction: Direction) {
+  const editor = vscode.window.activeTextEditor;
+
+  if (editor && editor.selection.isEmpty) {
+    const position = editor.selection.active;
+    const documentLines = iterLines(editor.document, position.line, direction);
+
+    for (const { prevLine, currentLine } of documentLines) {
+      if (lineIsInteresting(prevLine, currentLine)) {
+        moveCursorToBeginningOfLine(editor, currentLine);
+        break;
+      }
+    }
+  }
+}
+
+function moveToNextInterestingLine() {
+  return moveTo("forwards");
+}
+
+function moveToPrevInterestingLine() {
+  return moveTo("backwards");
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -9,76 +100,19 @@ export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "codeflea" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "extension.nextInterestingLine",
-    () => {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
-      const editor = vscode.window.activeTextEditor;
-
-      if (editor && editor.selection.isEmpty) {
-        // the Position object gives you the line and character where the cursor is
-        const position = editor.selection.active;
-
-        vscode.window.showInformationMessage("You're on line " + position.line);
-
-        function lineIsBoring(line: vscode.TextLine) {
-          return !/[a-zA-Z0-9]/.test(line.text);
-        }
-
-        function lineIsIndentationChange(
-          prevLine: vscode.TextLine,
-          currentLine: vscode.TextLine
-        ) {
-          return (
-            prevLine.firstNonWhitespaceCharacterIndex !==
-            currentLine.firstNonWhitespaceCharacterIndex
-          );
-        }
-
-        function lineIsInteresting(currentLine: vscode.TextLine) {
-          if (lineIsBoring(currentLine)) {
-            return false;
-          }
-
-          const prevLine = editor?.document.lineAt(currentLine.lineNumber - 1);
-
-          if (!prevLine) return false;
-
-          return (
-            lineIsIndentationChange(prevLine, currentLine) ||
-            lineIsBoring(prevLine)
-          );
-        }
-
-        var currentLineNumber = position.line;
-        var currentLine: vscode.TextLine | null = null;
-
-        while (currentLineNumber < editor.document.lineCount - 1) {
-          currentLineNumber++;
-
-          currentLine = editor.document.lineAt(currentLineNumber);
-
-          if (lineIsInteresting(currentLine)) {
-            break;
-          }
-        }
-
-        const column = currentLine?.firstNonWhitespaceCharacterIndex || 0;
-
-        editor.selection = new vscode.Selection(
-          new vscode.Position(currentLineNumber, column),
-          new vscode.Position(currentLineNumber, column)
-        );
-      }
-    }
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.nextInterestingLine",
+      moveToNextInterestingLine
+    )
   );
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.prevInterestingLine",
+      moveToPrevInterestingLine
+    )
+  );
 }
 
 // this method is called when your extension is deactivated
